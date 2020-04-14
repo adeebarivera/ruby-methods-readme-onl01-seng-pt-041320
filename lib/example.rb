a6c0d3ed8b600a751120916bb2678e4411163aba
adeeba reaz <adeebarivera@outlook.com>
2020-04-14 21:29:32 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -11,3 +11,58 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+
+
+def say_hello_world_ten_times
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
+Now, when we use the bareword
+say_hello_world_ten_times
+
+def say_hello_world_ten_times
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
+ 
+say_hello_world_ten_times
+
+ 
+# ... The rest of the program
+ 
+say_hello_world_ten_times
+
+def greeting # Method Signature
+  puts "Hello World" # Method Body
+end # Method Closing
+def greeting
+  puts "Hello World" # Now code the body of the method.
+end
+def greeting
+  puts "Hello World"
+end
+ 
+greeting # Executing the method by name
+#=> "Hello World"
+ 
+greeting # Executing the method again
+#=> "Hello World"
+Note: If you have been us
